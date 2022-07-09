6e1bd58d27dc5e460bcb9b1ddf5108879d1f7b35
Shrinivas Shukla <ssshukla1993@gmail.com>
2022-07-10 02:47:51 +0530

    Polling for schema changes every 5 sec

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const { ApolloServer, gql } = require("apollo-server");
 const { ApolloGateway, IntrospectAndCompose } = require("@apollo/gateway");
 const { readFileSync } = require("fs");
+const { ApolloServerPluginLandingPageGraphQLPlayground } =require("apollo-server-core");
 
 const gateway = new ApolloGateway({
+  debug: true,
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
       { name: "users", url: "https://gql-users-service.herokuapp.com/graphql" },
       { name: "posts", url: "https://gql-posts-service.herokuapp.com/graphql" },
+      // { name: "users", url: "http://localhost:8001/graphql" },
+      // { name: "posts", url: "http://localhost:8002/graphql" },
     ],
+    pollIntervalInMs: 5000,
   }),
+  // supergraphSdl: readFileSync("./supergraph.graphqls", {
+  //   encoding: "utf8",
+  //   flag: "r",
+  // }),
+  __exposeQueryPlanExperimental: true,
 });
 
 const server = new ApolloServer({
+  debug: true,
   gateway,
   introspection: true,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
